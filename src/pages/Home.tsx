6fdee12635e96bbc11f45cae925cc4de2a0b8b4f
Evan Bearman <evan.sportsman@gmail.com>
2024-09-24 13:41:32 -0600

    github pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import doorLogo from '../resources/doorlogo192.png'; // Adjust the path as necessary
+
 
 function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <img 
+        src={doorLogo} 
+        alt="Golden Door Logo" 
+        style={{ maxWidth: '100%', height: 'auto' }} 
+      />
     </div>
   );
 }
