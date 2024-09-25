f16adada46af2204f70a89273b8e1ef0eeb471de
Evan Bearman <evan.sportsman@gmail.com>
2024-09-24 21:27:46 -0600

    text added for hw

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,6 +4,11 @@ function About() {
   return (
     <div>
       <h1>Welcome to the About Page</h1>
+      <p>
+        Golden Door uses machine learning to find out which doors are the most valuable for sales teams that engage in door-to-door sales. 
+        Our AI technology identifies trends in where and how teams can improve their sales, eliminating the need for time-consuming research by team leads. 
+        This allows sales teams to focus on what they do best—closing deals—while we handle the data analysis and strategy.
+      </p>
     </div>
   );
 }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,6 +4,11 @@ function Contact() {
   return (
     <div>
       <h1>Welcome to the Contact Page</h1>
+      <p>If you'd like to reach us, you can:</p>
+      <ul>
+        <li><a href="mailto:bearmane@byu.edu?subject=Golden%20Door%20Inquiry&body=Please%20include%20your%20name%20and%20what%20and%20what%20you%20hope%20we%20can%20do%20for%20you.">Send Email</a></li>
+        <li>or <a href="https://docs.google.com/forms/d/1Z9WmKMq73SRc4oCdu1VjnTXnv-jYwzrjJ5kmU6lcWkI/prefill" target="_blank" rel="noopener noreferrer">click this link to fill out a survey</a>.</li>
+      </ul>
     </div>
   );
 }
